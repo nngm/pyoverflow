f968febd52943088c5d8242c981b081ffddb2635
nngm <andrea1010@naver.com>
2025-09-26 18:39:34 +0900

    Add iterator to PlayerArray

--- src/DSL/owglobals.py
+++ src/DSL/owglobals.py
@@ -10,5 +10,7 @@ class PlayerArray:
         return self._real
     def adopt(self, iterable) -> None:
         self._real = list(iterable)
+    def __iter__(self):
+        return iter(self._real)
 
 ALL_PLAYERS = PlayerArray()
