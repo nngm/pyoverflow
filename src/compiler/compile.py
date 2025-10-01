d24bc09519a8153b513d30f3e9fd1533888362f3
nngm <andrea1010@naver.com>
2025-10-02 01:28:51 +0900

    Make some compile skeleton

--- src/compiler/compile.py
+++ src/compiler/compile.py
@@ -1,8 +1,35 @@
 import argparse
 import os
 
+def parse():
+    pass
+
+def desugar():
+    pass
+
+def first_inlining():
+    pass
+
+def propagate_constant():
+    pass
+
+def lower_data_structure():
+    pass
+
+def build_code():
+    pass
+
+def optimize():
+    pass
+
 def compile(src_code: str) -> str:
-    tar_code = src_code
+    src_ast = parse(src_code)
+    IR = desugar(src_ast)
+    IR = first_inlining(IR)
+    IR = propagate_constant(IR)
+    IR = lower_data_structure(IR)
+    tar_code = build_code(IR)
+    tar_code = optimize(tar_code)
     return tar_code
 
 def main(src_path: str, tar_path: str):
