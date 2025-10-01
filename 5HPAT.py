6d8e523dd7638501c5c17c8dd3004373846f348d
nngm <andrea1010@naver.com>
2025-10-02 03:49:19 +0900

    Add workshop_setting

--- 5HPAT.py
+++ 5HPAT.py
@@ -59,8 +59,24 @@ class Global:
 
 
 @ongoing_global
-def init_global_variables():
-    Global.foo = custom_string("bar")
+def workshop_setting():
+    """Made by MAZAWATH"""
+    Global.number_p[0] = workshop_setting_integer("볼", "기본 속도", 40, 20, 100, 0)
+    Global.number_p[1] = workshop_setting_integer("볼", "최대 속도 (기본 속도 이상)", 60, 20, 300, 1)
+    Global.number_p[1] = max(first_of(Global.number_p), Global.number_p[1])
+    Global.number_p[4] = workshop_setting_integer("게임", "경기 시간", 1200, 30, 1200, 0)
+    Global.True_or_False[3] = workshop_setting_toggle("게임", "자동 경기 재시작", True, 1)
+    Global.True_or_False[9] = workshop_setting_toggle("게임", "워크샵 인스펙터 로그 (허용 시 서버 부하량 증가)", False, 2)
+    Global.True_or_False[7] = workshop_setting_combo("게임", "미니게임", 0, array("비활성화",
+        "겐지 주먹전"), 3)
+    Global.True_or_False[0] = workshop_setting_combo("게임", "겐지 주먹전에서 아래로 떨어지면", 0, array(
+        "즉사시켜 게임 관전 (겐지 주먹전 재입장 가능)", "다시 겐지 주먹전 진입"), 4)
+    Global.number_p[6] = workshop_setting_integer("봇", "최대 수", 1, 1, 11, 0)
+    Global.True_or_False[2] = workshop_setting_toggle("봇", "무빙 (비활성화 시 생성 버튼을 누른 자리에서 봇이 생성됩니다)", True,
+        1)
+    Global.True_or_False[5] = workshop_setting_toggle("봇", "에임 (비활성화 시 생성 버튼을 누른 시점의 에임으로 봇의 에임이 고정됩니다)",
+        True, 2)
+    Global.True_or_False[6] = workshop_setting_toggle("봇", "이름에 생성 번호 매기기", False, 3)
 
 @ongoing_each_player(ALL_TEAMS, ALL)
 @rules(
