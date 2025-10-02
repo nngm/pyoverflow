381c7ccb512ff0c9c33bc17deeb505151c035d96
nngm <andrea1010@naver.com>
2025-10-02 16:33:16 +0900

    Change rule compare example

--- main.py
+++ main.py
@@ -20,16 +20,8 @@ def init_global_variables():
 @ongoing_each_player(ALL_TEAMS, ALL)
 @rules(
     is_button_held(event_player, button(MELEE)),
-    compare(
-        count_of(
-            filtered_array(
-                all_players(ALL_TEAMS), 
-                CURRENT_ARRAY_ELEMENT.hasMoved
-            )
-        ), 
-        ">", 
-        1
-    )
+    count_of(filtered_array(all_players(ALL_TEAMS), CURRENT_ARRAY_ELEMENT.hasMoved))
+    > 1,
 )
 def let_player_minigame(event_player: player):
     event_player.Punch = True
