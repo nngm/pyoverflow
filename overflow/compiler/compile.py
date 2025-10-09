8e4900efc6a630259fc9a1e0cddb9632235fd51a
nngm <andrea1010@naver.com>
2025-10-10 02:05:08 +0900

    Start making compiler

--- overflow/compiler/compile.py
+++ overflow/compiler/compile.py
@@ -22,7 +22,7 @@ def build_code():
 def optimize():
     pass
 
-def compile(src_code: str) -> str:
+def compile(src_code: str, lang: str = "en") -> str:
     src_ast = parse(src_code)
     IR = desugar(src_ast)
     IR = first_inlining(IR)
@@ -32,23 +32,9 @@ def compile(src_code: str) -> str:
     tar_code = optimize(tar_code)
     return tar_code
 
-def main(src_path: str, tar_path: str):
+def main(src_path: str, tar_path: str, lang: str = "en"):
     with open(src_path, "r") as f:
         src_code = f.read()
-    tar_code = compile(src_code)
+    tar_code = compile(src_code, lang=lang)
     with open(tar_path, "w") as f:
         f.write(tar_code)
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("src_path", help="Source file path (e.g., main.py)")
-    parser.add_argument("-o", "--output", dest="tar_path",
-                        help="Output file path (optional). If not given, use input name with .ow")
-
-    args = parser.parse_args()
-
-    if args.tar_path is None:
-        base, _ = os.path.splitext(args.src_path)
-        args.tar_path = base + ".ow"
-
-    main(args.src_path, args.tar_path)
